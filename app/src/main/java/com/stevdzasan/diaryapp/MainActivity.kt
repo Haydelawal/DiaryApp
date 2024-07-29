f650584603324b0192bbce468c0419f2222a7c05
Abdul-Maatin Lawal <haydelawal@gmail.com>
2024-07-29 21:30:27 +0100

    added splash.xml (day and night) themes

--- app/src/main/java/com/stevdzasan/diaryapp/MainActivity.kt
+++ app/src/main/java/com/stevdzasan/diaryapp/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.stevdzasan.diaryapp.ui.theme.DiaryAppTheme
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DiaryAppTheme() {
             }
