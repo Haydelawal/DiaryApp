f865760a9cf9b48b62130ca2d02d9631c46bad88
Abdul-Maatin Lawal <haydelawal@gmail.com>
2024-08-09 20:09:31 +0100

    create diary and mood model classes

---
+++ app/src/main/java/com/hayde117/diaryapp/model/Diary.kt
@@ -0,0 +1,23 @@
+package com.hayde117.diaryapp.model
+
+import io.realm.kotlin.ext.realmListOf
+import io.realm.kotlin.types.ObjectId
+import io.realm.kotlin.types.RealmInstant
+import io.realm.kotlin.types.RealmList
+import io.realm.kotlin.types.RealmObject
+import io.realm.kotlin.types.annotations.PrimaryKey
+
+class Diary : RealmObject {
+
+    /** For Realm Object you have to declare them as var and this format in general, normal data class wont work **/
+
+    /** PRIMARY KEY REALM VERSION NOT ROOM DB VERSION **/
+    @PrimaryKey
+    var _id: ObjectId = ObjectId.create()
+    var ownerId: String = ""
+    var mood: String = Mood.Neutral.name
+    var title: String = ""
+    var description: String = ""
+    var images: RealmList<String> = realmListOf()
+    var date: RealmInstant = RealmInstant.from(System.currentTimeMillis(), 0)
+}
