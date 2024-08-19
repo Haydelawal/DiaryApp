beb2d2adee04adc0e13ca9af9dfb7556a9d0f6cd
Abdul-Maatin Lawal <haydelawal@gmail.com>
2024-08-20 00:38:15 +0100

    renamed libs.version.toml to libs.versions.toml, new build.gradle.kts and ProjectConfig.kt not working

---
+++ buidSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,11 @@
+object ProjectConfig {
+
+    // not working...
+
+    const val compileSdk = 33
+    const val minSdk = 33
+
+    const val targetSdk = 33
+    const val kotlinCompilerExtensionVersion = "1.3.1"
+
+}
